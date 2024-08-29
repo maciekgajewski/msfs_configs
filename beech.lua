e2a8bfe19c512b504ea470ca674a3aa2eaaee932
Maciek Gajewski <maciej.gajewski0@gmail.com>
2024-08-29 22:44:47 +0200

    renamed file, added hotas, implemented gear controls

--- beech.lua
+++ beech.lua
@@ -1,10 +1,43 @@
--- hotas = mapper.device{
---     name = 'Hotas',
---     type = 'dinput',
---  --   identifier = {name = 'Throttle - HOTAS Warthog'},
---     identifier = {name = 'Thrustmaster Combined'},
---     modifiers = {},
--- }
+hotas = mapper.device{
+    name = 'Hotas',
+    type = 'dinput',
+    identifier = {name = 'Throttle - HOTAS Warthog'},
+    modifiers = {
+        {name="button0", modtype="button"},
+        {name="button1", modtype="button"},
+        {name="button2", modtype="button"},
+        {name="button3", modtype="button"},
+        {name="button4", modtype="button"},
+        {name="button5", modtype="button"},
+        {name="button6", modtype="button"},
+        {name="button7", modtype="button"},
+        {name="button8", modtype="button"},
+        {name="button9", modtype="button"},
+        {name="button10", modtype="button"},
+        {name="button11", modtype="button"},
+        {name="button12", modtype="button"},
+        {name="button13", modtype="button"},
+        {name="button14", modtype="button"},
+        {name="button15", modtype="button"},
+        {name="button16", modtype="button"},
+        {name="button17", modtype="button"},
+        {name="button18", modtype="button"},
+        {name="button19", modtype="button"},
+        {name="button20", modtype="button"},
+        {name="button21", modtype="button"},
+        {name="button22", modtype="button"},
+        {name="button23", modtype="button"},
+        {name="button24", modtype="button"},
+        {name="button25", modtype="button"},
+        {name="button26", modtype="button"},
+        {name="button27", modtype="button"},
+        {name="button28", modtype="button"},
+        {name="button29", modtype="button"},
+        {name="button30", modtype="button"},
+        {name="button31", modtype="button"},
+        {name="button32", modtype="button"},
+    },
+}
 
 panel = mapper.device{
     name = 'Panel',
@@ -63,6 +96,7 @@ panel = mapper.device{
 }
 
 local panel_events = panel:get_events()
+local hotas_events = hotas:get_events()
 -- mapper.print("== Events BEGIN ==")
 -- for k,v in pairs(panel_events.button32) do
 --     mapper.print(k)
@@ -72,41 +106,55 @@ local panel_events = panel:get_events()
 -- local my_event = mapper.register_event('My Event')
 
 -- beech mappings
-selector_left = panel_events.button31
-selector_center = panel_events.button32
-selector_right = panel_events.button33
+selector_left = panel_events.button31.down
+selector_center = panel_events.button32.down
+selector_right = panel_events.button33.down
 
-primer_button = panel_events.button6
+primer_button = panel_events.button6.down
 
-left_fuel_sel_off = panel_events.button14
-left_fuel_sel_front = panel_events.button15
-left_fuel_sel_rear = panel_events.button16
+left_fuel_sel_off = panel_events.button14.down
+left_fuel_sel_front = panel_events.button15.down
+left_fuel_sel_rear = panel_events.button16.down
 
-right_fuel_sel_off = panel_events.button34
-right_fuel_sel_front = panel_events.button35
-right_fuel_sel_rear = panel_events.button36
+right_fuel_sel_off = panel_events.button34.down
+right_fuel_sel_front = panel_events.button35.down
+right_fuel_sel_rear = panel_events.button36.down
 
-left_mh_up = panel_events.button23
-left_mh_down = panel_events.button24
+left_mh_up = panel_events.button23.down
+left_mh_down = panel_events.button24.down
 
-right_mh_up = panel_events.button25
-right_mh_down = panel_events.button26
+right_mh_up = panel_events.button25.down
+right_mh_down = panel_events.button26.down
 
-left_os_up = panel_events.button27
-left_os_down = panel_events.button28
+left_os_up = panel_events.button27.down
+left_os_down = panel_events.button28.down
 
-right_os_up = panel_events.button29
-right_os_down = panel_events.button30
+right_os_up = panel_events.button29.down
+right_os_down = panel_events.button30.down
 
+left_fuel_pump_on = hotas_events.button16.down
+left_fuel_pump_off = hotas_events.button16.up
 
-left_fuel_pump_on = panel_events.button17
-left_fuel_pump_off = panel_events.button18
+right_fuel_pump_on = hotas_events.button17.down
+right_fuel_pump_off = hotas_events.button17.up
 
-right_fuel_pump_on = panel_events.button19
-right_fuel_pump_off = panel_events.button20
+hsi_cdi_inc = panel_events.button39.down
+hsi_cdi_dec = panel_events.button38.down
 
-hsi_cdi_inc = panel_events.button39
-hsi_cdi_dec = panel_events.button38
+parking_brake_on = hotas_events.button24.up
+parking_brake_off = hotas_events.button24.down
+
+gear_up = hotas_events.button25.down
+gear_down = hotas_events.button25.up
+
+tail_wheel_lock_toggle = hotas_events.button26.down
+
+landing_light_on = hotas_events.button27.down
+ -- middle swith pos can trigger two events, depneding on if its coming from up or down.
+ -- You can see how nicer the Virpil control is, compared to Thrustmaster
+landing_light_off = hotas_events.button27.up
+landing_light_off_alt = hotas_events.button28.up
+landing_light_retracted = hotas_events.button28.down
 
 -- state vars
 left_mh_val = 0
@@ -138,20 +186,20 @@ mapper.set_primary_mappings{
     -- },
 
     {
-        event = selector_left.down,
+        event = selector_left,
         action = msfs.mfwasm.rpn_executer('0 (>L:CARVAR_SW_STARTER_LR, Number) 0 (>L:CARVAR_SW_PRIMER_LR, Number)')
     },
     {
-        event = selector_center.down,
+        event = selector_center,
         action = msfs.mfwasm.rpn_executer('1 (>L:CARVAR_SW_STARTER_LR, Number) 1 (>L:CARVAR_SW_PRIMER_LR, Number)')
     },
     {
-        event = selector_right.down,
+        event = selector_right,
         action = msfs.mfwasm.rpn_executer('2 (>L:CARVAR_SW_STARTER_LR, Number) 2 (>L:CARVAR_SW_PRIMER_LR, Number)')
     },
     -- dont know how to operatre primer
     -- {
-    --     event = primer_button.down,
+    --     event = primer_button,
     --     --action =  msfs.mfwasm.rpn_executer('TRUE (>K:FUELSYSTEM_PUMP_ON:1, Bool)')
     --     -- action = function()
     --     --     msfs.send_event('FUELSYSTEM_PUMP_ON:1', 1)
@@ -162,21 +210,21 @@ mapper.set_primary_mappings{
 
     -- === Fuel boosters ==
     {
-        event = left_fuel_pump_on.down,
+        event = left_fuel_pump_on,
         action = msfs.mfwasm.rpn_executer('1 (>K:FUELSYSTEM_PUMP_ON)')
 
     },
     {
-        event = left_fuel_pump_off.down,
+        event = left_fuel_pump_off,
         action = msfs.mfwasm.rpn_executer('1 (>K:FUELSYSTEM_PUMP_OFF)')
     },
     {
-        event = right_fuel_pump_on.down,
+        event = right_fuel_pump_on,
         action = msfs.mfwasm.rpn_executer('2 (>K:FUELSYSTEM_PUMP_ON)')
 
     },
     {
-        event = right_fuel_pump_off.down,
+        event = right_fuel_pump_off,
         action = msfs.mfwasm.rpn_executer('2 (>K:FUELSYSTEM_PUMP_OFF)')
     },
 
@@ -184,28 +232,28 @@ mapper.set_primary_mappings{
     -- == Fuel selectors ==
 
     {
-        event = left_fuel_sel_off.down,
+        event = left_fuel_sel_off,
         action = msfs.mfwasm.rpn_executer('0 (>L:Denaq_Fuel_Sel1, Number) 3 1 (>K:2:FUELSYSTEM_JUNCTION_SET)'),
     },
     {
-        event = left_fuel_sel_front.down,
+        event = left_fuel_sel_front,
         action = msfs.mfwasm.rpn_executer('1 (>L:Denaq_Fuel_Sel1, Number) 1 1 (>K:2:FUELSYSTEM_JUNCTION_SET)'),
     },
     {
-        event = left_fuel_sel_rear.down,
+        event = left_fuel_sel_rear,
         action = msfs.mfwasm.rpn_executer('2 (>L:Denaq_Fuel_Sel1, Number) 2 1 (>K:2:FUELSYSTEM_JUNCTION_SET)'),
     },
 
     {
-        event = right_fuel_sel_off.down,
+        event = right_fuel_sel_off,
         action = msfs.mfwasm.rpn_executer('0 (>L:Denaq_Fuel_Sel2, Number) 3 2 (>K:2:FUELSYSTEM_JUNCTION_SET)'),
     },
     {
-        event = right_fuel_sel_front.down,
+        event = right_fuel_sel_front,
         action = msfs.mfwasm.rpn_executer('1 (>L:Denaq_Fuel_Sel2, Number) 1 2 (>K:2:FUELSYSTEM_JUNCTION_SET)'),
     },
     {
-        event = right_fuel_sel_rear.down,
+        event = right_fuel_sel_rear,
         action = msfs.mfwasm.rpn_executer('2 (>L:Denaq_Fuel_Sel2, Number) 2 2 (>K:2:FUELSYSTEM_JUNCTION_SET)'),
     },
 
@@ -213,7 +261,7 @@ mapper.set_primary_mappings{
     -- === De-ice ==
 
     {
-        event = left_mh_up.down,
+        event = left_mh_up,
         --action = msfs.mfwasm.rpn_executer('5000 (>L:ANTI-ICE-GRADUAL-SET-ENG1, Number)'), doesnt work
 
         -- this works - sets a value
@@ -232,7 +280,7 @@ mapper.set_primary_mappings{
         end
     },
     {
-        event = left_mh_down.down,
+        event = left_mh_down,
         action = function ()
             if left_mh_val < 16384 then 
                 left_mh_val = left_mh_val + mh_increment
@@ -241,7 +289,7 @@ mapper.set_primary_mappings{
         end
     },
     {
-        event = right_mh_up.down,
+        event = right_mh_up,
         action = function ()
             if right_mh_val > 0 then 
                 right_mh_val = right_mh_val - mh_increment
@@ -250,7 +298,7 @@ mapper.set_primary_mappings{
         end
     },
     {
-        event = right_mh_down.down,
+        event = right_mh_down,
         action = function ()
             if right_mh_val < 16384 then 
                 right_mh_val = right_mh_val + mh_increment
@@ -261,7 +309,7 @@ mapper.set_primary_mappings{
 
     -- == Oil Shutters ==
     {
-        event = left_os_up.down,
+        event = left_os_up,
         action = function ()
             if left_os_val > 0 then 
                 left_os_val = left_os_val - os_increment
@@ -272,7 +320,7 @@ mapper.set_primary_mappings{
         end
     },
     {
-        event = left_os_down.down,
+        event = left_os_down,
         action = function ()
             if left_os_val < 100 then 
                 left_os_val = left_os_val + os_increment
@@ -281,7 +329,7 @@ mapper.set_primary_mappings{
         end
     },
     {
-        event = right_os_up.down,
+        event = right_os_up,
         action = function ()
             if right_os_val > 0 then 
                 right_os_val = right_os_val - os_increment
@@ -290,7 +338,7 @@ mapper.set_primary_mappings{
         end
     },
     {
-        event = right_os_down.down,
+        event = right_os_down,
         action = function ()
             if right_os_val < 100 then 
                 right_os_val = right_os_val + os_increment
@@ -302,13 +350,35 @@ mapper.set_primary_mappings{
     -- === HSI ===
     -- increment with single click, increase by 4 when clicking fast
     {
-        event = hsi_cdi_inc.down,
+        event = hsi_cdi_inc,
         action = msfs.mfwasm.rpn_executer('(E:SIMULATION TIME,Number) (L:MACIEK_LAST_VOR_OBI_TIME, Number) - 0.5 < if{  1 (>K:VOR1_OBI_INC) 1 (>K:VOR1_OBI_INC) 1 (>K:VOR1_OBI_INC) } 1 (>K:VOR1_OBI_INC) (E:SIMULATION TIME,Number) (>L:MACIEK_LAST_VOR_OBI_TIME)') -- HAHA works!
     },
     {
-        event = hsi_cdi_dec.down,
+        event = hsi_cdi_dec,
         action = msfs.mfwasm.rpn_executer('(E:SIMULATION TIME,Number) (L:MACIEK_LAST_VOR_OBI_TIME, Number) - 0.5 < if{  1 (>K:VOR1_OBI_DEC) 1 (>K:VOR1_OBI_DEC) 1 (>K:VOR1_OBI_DEC) } 1 (>K:VOR1_OBI_DEC) (E:SIMULATION TIME,Number) (>L:MACIEK_LAST_VOR_OBI_TIME)') -- HAHA works!
     },
 
+    -- wheels
+    {
+        event = parking_brake_on,
+        action =  msfs.mfwasm.rpn_executer('1 (>K:PARKING_BRAKE_SET,Bool)')
+    },
+    {
+        event = parking_brake_off,
+        action =  msfs.mfwasm.rpn_executer('0 (>K:PARKING_BRAKE_SET,Bool)')
+    },
+    {
+        event = gear_up,
+        action =  msfs.mfwasm.rpn_executer('(>K:GEAR_UP)')
+    },
+    {
+        event = gear_down,
+        action =  msfs.mfwasm.rpn_executer('(>K:GEAR_DOWN)')
+    },
+    {
+        event = tail_wheel_lock_toggle,
+        action =  msfs.mfwasm.rpn_executer('(>K:TOGGLE_TAILWHEEL_LOCK)')
+    },
+
+
 }
--- mapper.raise_event(my_event, 1)
