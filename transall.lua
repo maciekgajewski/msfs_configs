37f0cc9909bdcb23f267be9735cf5a18fb223f51
Maciej Gajewski <maciej.gajewski0@gmail.com>
2026-03-11 19:25:35 +0100

    battery events

--- transall.lua
+++ transall.lua
@@ -10,6 +10,45 @@ flaps_mid = stecs_events.button61.up
 flaps_mid_alt = stecs_events.button62.up
 flaps_down = stecs_events.button62.down
 
+ap_master_toggle = stecs_events.button10.down
+
+ap_heading_pitch_dec = stecs_events.button29.down
+ap_heading_pitch_inc = stecs_events.button30.down
+ap_heading_pitch_enc_dec = stecs_events.button55.down
+ap_heading_pitch_enc_inc = stecs_events.button56.down
+
+ap_vs_inc = stecs_events.button32.down
+ap_vs_dec = stecs_events.button31.down
+
+ap_hold_heading_toggle = stecs_events.button38.down
+ap_hold_altitute_toggle = stecs_events.button37.down
+ap_hold_pitch_toggle = stecs_events.button35.down
+ap_hold_nav_toggle = stecs_events.button36.down
+
+hsi_cdi_inc = stecs_events.button58.down
+hsi_cdi_dec = stecs_events.button57.down
+
+-- lights
+landing_light_left_off = panel_events.button27.down
+landing_light_left_on = panel_events.button28.down
+
+landing_light_right_off = panel_events.button29.down
+landing_light_right_on = panel_events.button30.down
+
+taxi_lights_off = panel_events.button25.down
+taxi_lights_on = panel_events.button26.down
+
+-- Electrical
+
+battery_1_toggle = stecs_events.button39.down
+battery_2_toggle = stecs_events.button40.down
+
+generator_1_toggle = stecs_events.button42.down
+generator_2_toggle = stecs_events.button43.down
+generator_3_toggle = stecs_events.button44.down
+generator_4_toggle = stecs_events.button45.down
+generator_5_toggle = stecs_events.button46.down
+
 transall_mappings = {
 
 
@@ -27,22 +66,116 @@ transall_mappings = {
         action =  msfs.mfwasm.rpn_executer('(>K:GEAR_DOWN)')
     },
 
-    -- == Flaps ==
+        -- AP --
     {
-        event = flaps_up,
-        action =  msfs.mfwasm.rpn_executer('(>K:FLAPS_UP)')
+        event = ap_master_toggle,
+        action =  msfs.mfwasm.rpn_executer('(>K:AP_MASTER)')
     },
     {
-        event = flaps_mid,
-        action =  msfs.mfwasm.rpn_executer('(>K:FLAPS_1)')
+        event = ap_hold_heading_toggle,
+        action =  msfs.mfwasm.rpn_executer('(>K:AP_PANEL_HEADING_HOLD)')
     },
     {
-        event = flaps_mid_alt,
-        action =  msfs.mfwasm.rpn_executer('(>K:FLAPS_1)')
+        event = ap_hold_nav_toggle,
+        action =  msfs.mfwasm.rpn_executer('(>K:AP_APR_HOLD)')
     },
     {
-        event = flaps_down,
-        action =  msfs.mfwasm.rpn_executer('(>K:FLAPS_DOWN)')
+        event = ap_hold_altitute_toggle,
+        action =  msfs.mfwasm.rpn_executer('(>K:AP_ALT_HOLD)')
     },
-    
+    {
+        event = ap_hold_pitch_toggle,
+        action =  msfs.mfwasm.rpn_executer('(>K:AP_PANEL_VS_HOLD)')
+    },
+    {
+        event = ap_heading_pitch_dec,
+        action =  msfs.mfwasm.rpn_executer('(>K:HEADING_BUG_DEC)')
+    },
+    {
+        event = ap_heading_pitch_inc,
+        action =  msfs.mfwasm.rpn_executer('(>K:HEADING_BUG_INC)')
+    },
+    {
+        event = ap_heading_pitch_enc_dec,
+        action =  msfs.mfwasm.rpn_executer('(>K:HEADING_BUG_DEC)')
+    },
+    {
+        event = ap_heading_pitch_enc_inc,
+        action =  msfs.mfwasm.rpn_executer('(>K:HEADING_BUG_INC)')
+    },
+    {
+        event = ap_vs_inc,
+        action =  msfs.mfwasm.rpn_executer('(>K:AP_VS_VAR_INC)')
+    },
+    {
+        event = ap_vs_dec,
+        action =  msfs.mfwasm.rpn_executer('(>K:AP_VS_VAR_DEC)')
+    },
+{
+        event = hsi_cdi_inc,
+        action = msfs.mfwasm.rpn_executer('1 (>K:VOR1_OBI_INC)')
+    },
+    {
+        event = hsi_cdi_dec,
+        action = msfs.mfwasm.rpn_executer('1 (>K:VOR1_OBI_DEC)') 
+    },
+
+        -- == Lights == --
+    {
+        event = landing_light_left_off,
+        action = msfs.mfwasm.rpn_executer('1 (>K:2:LANDING_LIGHTS_OFF)')
+    },
+    {
+        event = landing_light_left_on,
+        action = msfs.mfwasm.rpn_executer('1 (>K:2:LANDING_LIGHTS_ON)')
+    },
+    {
+        event = landing_light_right_off,
+        action = msfs.mfwasm.rpn_executer('2 (>K:2:LANDING_LIGHTS_OFF)')
+    },
+    {
+        event = landing_light_right_on,
+        action = msfs.mfwasm.rpn_executer('2 (>K:2:LANDING_LIGHTS_ON)')
+    },
+    {
+        event = taxi_lights_off,
+        --action = msfs.mfwasm.rpn_executer('(>K:TAXI_LIGHTS_OFF)')
+        action = msfs.mfwasm.rpn_executer('1 (>L:AZP_C160_TAXI_LIGHT_SWITCH)')
+    },
+    {
+        event = taxi_lights_on,
+        action = msfs.mfwasm.rpn_executer('2 (>L:AZP_C160_TAXI_LIGHT_SWITCH)')
+    },
+
+    -- electrical
+    {
+        event = battery_1_toggle,
+        action = msfs.mfwasm.rpn_executer('(L:AZP_C160_BAT1_GPU_SWITCH) 2 == if{ (>H:AZP_C160_SOURCE1_OFF) } els{ (>H:AZP_C160_SOURCE1_BAT1) }')
+        
+    },
+    {
+        event = battery_2_toggle,
+        action = msfs.mfwasm.rpn_executer('(L:AZP_C160_BAT2_SWITCH) 1 == if{ (>H:AZP_C160_SOURCE2_OFF) } els{ (>H:AZP_C160_SOURCE2_ON) }')
+    },
+    {
+        event = generator_1_toggle,
+        action = msfs.mfwasm.rpn_executer('(>L:AZP_C160_G1_KNOB) ! (>L:AZP_C160_G1_KNOB)')
+    },
+    {
+        event = generator_2_toggle,
+        action = msfs.mfwasm.rpn_executer('(>L:AZP_C160_G2_KNOB) ! (>L:AZP_C160_G2_KNOB)')
+    },
+    {
+        event = generator_3_toggle,
+        action = msfs.mfwasm.rpn_executer('(>L:AZP_C160_G3_KNOB) ! (>L:AZP_C160_G3_KNOB)')
+    },
+    {
+        event = generator_4_toggle,
+        action = msfs.mfwasm.rpn_executer('(>L:AZP_C160_G4_KNOB) ! (>L:AZP_C160_G4_KNOB)')
+    },
+    {
+        event = generator_5_toggle,
+        action = msfs.mfwasm.rpn_executer('(>L:AZP_C160_G5_KNOB) ! (>L:AZP_C160_G5_KNOB)')
+    }
+
 }
