cfc0486fa6ec3797a1eb48562da0d8f4a0ddc0e8
Maciej Gajewski <maciej.gajewski0@gmail.com>
2026-06-28 15:56:07 +0200

    engine vanes and more ap modes

--- starship.lua
+++ starship.lua
@@ -24,8 +24,8 @@ ap_vs_enc_dec = panel_events.button38.down
 ap_vs_enc_inc = panel_events.button39.down
 ap_vs_enc_push = panel_events.button37.down
 
-ap_alt_enc_dec = panel_events.button42.down
-ap_alt_enc_inc = panel_events.button41.down
+ap_alt_enc_inc = panel_events.button42.down
+ap_alt_enc_dec = panel_events.button41.down
 ap_alt_enc_push = panel_events.button40.down
 
 ap_mode_alt_hold = stecs_events.button37.down
@@ -34,6 +34,8 @@ ap_mode_hdg = stecs_events.button38.down
 ap_mode_nav = stecs_events.button36.down
 ap_mode_apr = stecs_events.button19.down
 ap_disengage = stecs_events.button10.down
+ap_climb = stecs_events.button25.down
+ap_descend = stecs_events.button26.down
 
 -- AP switches
 ap_on = stecs_events.button50.down
@@ -49,6 +51,11 @@ lights_button_nav = panel_events.button11.down
 lights_button_strobe_low = panel_events.button12.down
 lights_button_anti_collision = panel_events.button13.down
 
+-- de-ice
+de_ice_right_engine_on = panel_events.button14.down
+de_ice_right_engine_off = panel_events.button16.down
+de_ice_left_engine_on = panel_events.button34.down
+de_ice_left_engine_off = panel_events.button36.down 
 
 starship_mappings = {
 
@@ -164,12 +171,16 @@ starship_mappings = {
         event = ap_disengage,
         action =  msfs.mfwasm.rpn_executer('(>K:AUTOPILOT_DISENGAGE_TOGGLE)')
     },
+    {
+        event = ap_climb,
+        action =  msfs.mfwasm.rpn_executer('(>H:MSP_AirspeedProfile_1)')
+    },
+    {
+        event = ap_descend,
+        action =  msfs.mfwasm.rpn_executer('(>H:MSP_Descend_1)')
+    },
 
     -- AP --
--- ap_on = stecs_events.button50.down
--- ap_off = stecs_events.button42.down
--- yaw_dampener_on = stecs_events.button47.down
--- yaw_dampener_off = stecs_events.button49.down
     {
         event = ap_on,
         action =  msfs.mfwasm.rpn_executer('(A:AUTOPILOT MASTER, Bool) ! if{ (>K:AP_MASTER) }')
@@ -189,21 +200,8 @@ starship_mappings = {
 
 
     -- External Lights panel --
--- upper light panel
--- lights_button_landing = panel_events.button8.down
--- lights_button_taxi = panel_events.button9.down
--- light_button_wing = panel_events.button10.down
--- lights_button_nav = panel_events.button11.down
--- lights_button_strobe_low = panel_events.button12.down
--- lights_button_anti_collision = panel_events.button13.down
     {
         event = lights_button_landing,
-        -- works funnu: swithces dirst wing, then nose
-        --action = msfs.mfwasm.rpn_executer('(A:LIGHT LANDING, Bool) ! if{ 1 1 (>K:2:LANDING_LIGHTS_SET) 1 2 (>K:2:LANDING_LIGHTS_SET) } els{ 0 1 (>K:2:LANDING_LIGHTS_SET) 0 2 (>K:2:LANDING_LIGHTS_SET) }')
-        
-        -- toggles  only wing
-        --action = msfs.mfwasm.rpn_executer('(A:LIGHT LANDING, Bool) ! if{ 1 1 (>K:2:LANDING_LIGHTS_SET) 2 1 (>K:2:LANDING_LIGHTS_SET) } els{ 1 0 (>K:2:LANDING_LIGHTS_SET) 2 0 (>K:2:LANDING_LIGHTS_SET) }')
-
         action = msfs.mfwasm.rpn_executer('(A:LIGHT LANDING, Bool) ! if{ 1 1 (>K:2:LANDING_LIGHTS_SET) 0 1 (>K:2:LANDING_LIGHTS_SET) } els{ 1 0 (>K:2:LANDING_LIGHTS_SET) 0 0 (>K:2:LANDING_LIGHTS_SET) }')
     },
     {
@@ -227,6 +225,23 @@ starship_mappings = {
         action = msfs.mfwasm.rpn_executer('(L:var_StrobeLight_High, Bool) ! (>L:var_StrobeLight_High, Bool)')
     },
 
+    -- de-ice
+    {
+        event = de_ice_left_engine_on,
+        action = msfs.mfwasm.rpn_executer('1 (>L:var_InertialSeparatorSwitch_R)')
+    },
+    {
+        event = de_ice_left_engine_off,
+        action = msfs.mfwasm.rpn_executer('0 (>L:var_InertialSeparatorSwitch_R)')
+    },
+    {
+        event = de_ice_right_engine_on,
+        action = msfs.mfwasm.rpn_executer('1 (>L:var_InertialSeparatorSwitch_L)')
+    },
+    {
+        event = de_ice_right_engine_off,
+        action = msfs.mfwasm.rpn_executer('0 (>L:var_InertialSeparatorSwitch_L)')
+    },
 
 
 }
